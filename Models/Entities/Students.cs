da92bd40b20be984bb624d6413ea7b21a991ce54
Mathys <mathys.ledanois@viacesi.fr>
2024-08-27 10:53:40 +0200

    authentification

--- Models/Entities/Students.cs
+++ Models/Entities/Students.cs
@@ -3,7 +3,10 @@
     public class Students
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
